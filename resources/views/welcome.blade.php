1ac8eaea8e5672dbaae575e6b584c83fb89e7f8d
syafiqshamsuddin96 <syafiqshamsuddin96@gmail.com>
2019-12-10 18:22:00 +0800

    generated auth and scaffold blog

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -80,7 +80,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Blog
                 </div>
 
                 <div class="links">
